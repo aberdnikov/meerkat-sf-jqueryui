10c52acf3f01c712f252fd4b68850fe545173be4
Alexey Berdnikov <aberdnikov@gmail.com>
2014-06-04 21:45:15 +0600

    fix

--- config/meerkat/jqueryui.php
+++ config/meerkat/jqueryui.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-    'version' => 'lib/jqueryui/js/jquery-ui-1.10.3.custom.min.js',
+    'version' => 'lib/jqueryui/js/jquery-ui-1.10.4.custom.js',
     'theme' => 'lib/jqueryui/themes/ui-lightness/jquery-ui.css'
 );
